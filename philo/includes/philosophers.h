19056032c1788a2bb8c067d25361e3065974625e
KrebsCoder <lkrebs-l@student.42sp.org.br>
2022-06-27 20:06:33 +0200

    fix: norm

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -3,24 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 22:54:36 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/06/26 00:11:47 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/06/27 20:06:12 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/time.h>
-#include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <string.h>
 
-typedef struct s_philo {
+typedef struct s_philo
+{
 	int	nbr_philos;
 	int	nbr_forks;
 	int	time_eat;
@@ -34,6 +35,6 @@ void	check_arguments(int argc, char *argv[]);
 // Utils
 int		ft_atoi(const char *str);
 void	ft_init(t_philo *ph, int argc, char *argv[]);
-int		is_str_positive_num(char	*str);
+int		is_str_positive_num(char *str);
 
 #endif
