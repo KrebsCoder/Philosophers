955e8c2a6c0e10b6203e348a5d086d5cd8ceeabb
gabrielasertori <gabriela@sertori.com.br>
2022-07-06 04:34:13 +0200

    add: routines and philosophers

--- philo/source/create_philo.c
+++ philo/source/create_philo.c
@@ -6,30 +6,85 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 02:06:00 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/07/05 18:46:50 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/06 04:19:12 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
+static void	create_threads(t_philo *philo);
+static void	thread_join(t_philo *philo);
+static void	init_linked_list(t_philo *philo);
+static void	init_mutex(t_philo *philo);
+
 void	create_philo(t_philo *philo)
+{
+	init_linked_list(philo);
+	init_mutex(philo);
+	create_threads(philo);
+	thread_join(philo);
+	pthread_mutex_destroy(&philo->list->mutex);
+	ft_exit(philo->list);
+}
+
+static void	thread_join(t_philo *philo)
+{
+	t_list *tmp;
+	int	i;
+
+	i = philo->nbr_forks;
+	tmp = philo->list;
+	while (i > 0)
+	{
+		pthread_join(tmp->thread, NULL);
+		tmp = tmp->next;
+		i--;
+	}
+}
+
+static void	init_mutex(t_philo *philo)
+{
+	t_list *tmp;
+	int		i;
+
+	i = philo->nbr_philos;
+	tmp = philo->list;
+	while (i > 0)
+	{
+		pthread_mutex_init(&tmp->mutex, NULL);
+		printf("current: %d\n", tmp->id);
+		printf("previous: %d\nnext: %d\n", tmp->previous->id, tmp->next->id);
+		tmp = tmp->next;
+		i--;
+	}
+}
+
+static void	create_threads(t_philo *philo)
+{
+	t_list *tmp;
+	int		i;
+
+	i = philo->nbr_philos;
+	tmp = philo->list;
+	while (i > 0)
+	{
+		pthread_create(&tmp->thread, NULL, &routine, tmp);
+		tmp = tmp->next;
+		i--;
+	}
+}
+
+static void	init_linked_list(t_philo *philo)
 {
 	t_list	*node;
 	int		i;
 
-	i = 0;
-	while (i < philo->nbr_philos)
+	i = philo->nbr_philos - 1;
+	philo->list = ft_lstnew(0, philo);
+	while (i > 0)
 	{
-		node = ft_lstnew(i);
-		ft_lstadd_back(&philo->list, node);
-		pthread_create(&node->thread, NULL, &routine, node);
-		i++;
+		node = ft_lstnew(i, philo);
+		ft_lstadd_back(philo->list, node);
+		i--;
 	}
-	i = 0;
-	while (i < philo->nbr_philos)
-	{
-		pthread_join(node->thread, NULL);
-		i++;
-	}
-	ft_exit(philo->list);
 }
--- philo/source/routines.c
+++ philo/source/routines.c
@@ -6,40 +6,48 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 02:33:19 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/05 18:45:31 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/06 04:21:55 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-static void	philo_is_eating(t_list *ph);
-/* static void	philo_is_sleeping(void);
-static void	philo_is_thinking(void); */
+static void	philo_is_eating(t_list *philo);
+static void	philo_is_sleeping(t_list *philo);
+static void	philo_is_thinking(t_list *philo);
 
 void	*routine(void *node)
 {
 	t_list *philo = (t_list *)node;
 
+	gettimeofday(&philo->current_time, NULL);
+	printf("%ld\n", philo->current_time.tv_usec * 1000);
 	while (1)
 	{
 		philo_is_eating(philo);
-		/* philo_is_sleeping();
-		philo_is_thinking(); */
+		philo_is_sleeping(philo);
+		philo_is_thinking(philo);
 	}
 	return (NULL);
 }
 
 static void	philo_is_eating(t_list *philo)
 {
-	printf("tempo_em_ms filosofo %d está comendo\n", philo->id);
+	pthread_mutex_lock(&philo->mutex);
+	pthread_mutex_lock(&philo->next->mutex);
+	printf("tempo em ms ms filosofo %d está comendo\n", philo->id);
+	usleep(philo->philo->time_eat * 1000);
+	pthread_mutex_unlock(&philo->mutex);
+	pthread_mutex_unlock(&philo->next->mutex);
 }
 
-/* static void	philo_is_sleeping(void)
+static void	philo_is_sleeping(t_list *philo)
 {
-	printf("filosofo x está dormindo\n");
+	printf("filosofo %d está dormindo\n", philo->id);
+	usleep(philo->philo->time_sleep * 1000);
 }
 
-static void	philo_is_thinking(void)
+static void	philo_is_thinking(t_list *philo)
 {
-	printf("filosofo x está pensando\n");
-} */
+	printf("filosofo %d está pensando\n", philo->id);
+}
