7bbca400ca7fb2fa471afafb2891f8580e69bcfb
gabrielasertori <gabriela@sertori.com.br>
2022-07-06 04:31:09 +0200

    modify: struct name

--- philo/utils/ft_exit.c
+++ philo/utils/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 02:21:53 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/07/05 18:47:32 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/06 02:38:49 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void free_list(t_list *philo)
 	t_list *node;
 	int	i;
 
-	i = philo->ph->nbr_philos;
+	i = philo->philo->nbr_philos;
 	
 	while (i > 0)
 	{
@@ -30,6 +30,7 @@ static void free_list(t_list *philo)
 
 void	ft_exit(t_list *list)
 {
+
 	free_list(list);
 	exit(0);
 }
--- philo/utils/ft_init.c
+++ philo/utils/ft_init.c
@@ -3,26 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 23:02:40 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/06/29 02:20:56 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/06 03:48:18 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	ft_init(t_philo *ph, int argc, char *argv[])
+void	ft_init(t_philo *philo, int argc, char *argv[])
 {
 	check_arguments(argc, argv);
-	ph->nbr_philos = ft_atoi(argv[1]);
-	ph->nbr_forks = ph->nbr_philos;
-	ph->time_die = ft_atoi(argv[2]);
-	ph->time_eat = ft_atoi(argv[3]);
-	ph->time_sleep = ft_atoi(argv[4]);
-	ph->list = malloc (sizeof(t_list) * 1);
+	philo->nbr_philos = ft_atoi(argv[1]);
+	philo->nbr_forks = philo->nbr_philos;
+	philo->time_die = ft_atoi(argv[2]);
+	philo->time_eat = ft_atoi(argv[3]);
+	philo->time_sleep = ft_atoi(argv[4]);
 	if (argv[5])
-		ph->times_must_eat = ft_atoi(argv[5]);
+		philo->times_must_eat = ft_atoi(argv[5]);
 	else
-		ph->times_must_eat = 0;
+		philo->times_must_eat = 0;
 }
