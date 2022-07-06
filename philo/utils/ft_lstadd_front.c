476e920342e8a49ca43e9d4a2b5231abf433bf7b
gabrielasertori <gabriela@sertori.com.br>
2022-07-06 04:29:29 +0200

    modify: to circular double linked list

--- philo/utils/ft_lstadd_front.c
+++ philo/utils/ft_lstadd_front.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/05 01:21:48 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/06 03:06:07 by gcosta-d         ###   ########.fr       */
+/*   Created: 2022/07/05 01:25:06 by gcosta-d          #+#    #+#             */
+/*   Updated: 2022/07/06 04:22:54 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-t_list	*ft_lstnew(int id, t_philo *philo)
+void	ft_lstadd_front(t_list *lst, t_list *new)
 {
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	if (!new)
-		return (NULL);
-	new -> philo = philo;
-	new -> id = id;
-	new -> next = new;
-	new -> previous = new;
-	return (new);
+	if (lst == NULL)
+		lst = new;
+	else
+	{
+		new -> next = lst->next;
+		new -> previous = lst;
+		lst->next->previous = new;
+		lst->next = new;
+		lst = new;
+	}
 }
