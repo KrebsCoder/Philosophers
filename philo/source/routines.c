e965b30409c6565699665476fbe4b5e10d896f14
KrebsCoder <lucasklopess@gmail.com>
2022-07-07 04:41:12 +0200

    remove: routines from routines file, now each routine has its own file

--- philo/source/routines.c
+++ philo/source/routines.c
@@ -6,22 +6,17 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 02:33:19 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/06 04:21:55 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/07 03:46:47 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-static void	philo_is_eating(t_list *philo);
-static void	philo_is_sleeping(t_list *philo);
-static void	philo_is_thinking(t_list *philo);
-
 void	*routine(void *node)
 {
-	t_list *philo = (t_list *)node;
+	t_list	*philo = (t_list *)node;
 
-	gettimeofday(&philo->current_time, NULL);
-	printf("%ld\n", philo->current_time.tv_usec * 1000);
+	gettimeofday(&philo->philo->start_time, NULL);
 	while (1)
 	{
 		philo_is_eating(philo);
@@ -29,25 +24,4 @@ void	*routine(void *node)
 		philo_is_thinking(philo);
 	}
 	return (NULL);
-}
-
-static void	philo_is_eating(t_list *philo)
-{
-	pthread_mutex_lock(&philo->mutex);
-	pthread_mutex_lock(&philo->next->mutex);
-	printf("tempo em ms ms filosofo %d está comendo\n", philo->id);
-	usleep(philo->philo->time_eat * 1000);
-	pthread_mutex_unlock(&philo->mutex);
-	pthread_mutex_unlock(&philo->next->mutex);
-}
-
-static void	philo_is_sleeping(t_list *philo)
-{
-	printf("filosofo %d está dormindo\n", philo->id);
-	usleep(philo->philo->time_sleep * 1000);
-}
-
-static void	philo_is_thinking(t_list *philo)
-{
-	printf("filosofo %d está pensando\n", philo->id);
 }
