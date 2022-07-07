7349108b14e970e750f98f2d2585e8a4c1d8189d
KrebsCoder <lucasklopess@gmail.com>
2022-07-07 04:37:33 +0200

    add: new files to header and removed a few vars from list

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 22:54:36 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/06 04:25:01 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/07 03:51:46 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,40 @@
 
 typedef struct s_list
 {
-	pthread_mutex_t	mutex;
+	pthread_mutex_t	fork;
 	pthread_t		thread;
+	int				eat_counter;
 	int				id;
-	long			time;
-	long			time_start;
-	struct	timeval current_time;
-	struct	s_list *previous;
-	struct	s_list *next;
+	struct	s_list	*previous;
+	struct	s_list	*next;
 	struct	s_philo	*philo;
 }	t_list;
 
 typedef struct s_philo
 {
-	int		nbr_philos;
-	int		nbr_forks;
-	int		time_eat;
-	int		time_sleep;
-	int		time_die;
-	int		times_must_eat;
-	t_list	*list;
+	int				nbr_philos;
+	int				nbr_forks;
+	int				time_eat;
+	int				time_sleep;
+	int				time_die;
+	int				times_must_eat;
+	struct	timeval	start_time;
+	struct	timeval	current_time;
+	t_list			*list;
 }	t_philo;
-
 
 void	check_arguments(int argc, char *argv[]);
 void	create_philo(t_philo *ph);
 void	*routine(void *philo);
+void	create_threads(t_philo *philo);
+void	thread_join(t_philo *philo);
+void	init_linked_list(t_philo *philo);
+void	init_mutex(t_philo *philo);
+long	time_in_ms(t_list *philo);
+
+void	philo_is_eating(t_list *philo);
+void	philo_is_sleeping(t_list *philo);
+void	philo_is_thinking(t_list *philo);
 
 // Utils
 int		ft_atoi(const char *str);
