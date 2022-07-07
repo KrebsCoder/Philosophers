ca637d80fb5c61a960ea17e3d266d2899c2d62d0
KrebsCoder <lucasklopess@gmail.com>
2022-07-07 04:42:32 +0200

    add: function to initialize the linked list

---
+++ philo/source/init_linked_list.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_linked_list.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 02:22:44 by gcosta-d          #+#    #+#             */
+/*   Updated: 2022/07/07 02:23:13 by gcosta-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+void	init_linked_list(t_philo *philo)
+{
+	t_list	*node;
+	int		i;
+
+	i = philo->nbr_philos - 1;
+	philo->list = ft_lstnew(0, philo);
+	while (i > 0)
+	{
+		node = ft_lstnew(i, philo);
+		ft_lstadd_front(philo->list, node);
+		i--;
+	}
+}
