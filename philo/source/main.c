453ddfe44b06111a2fd9ed7ba3d0eb9d8e7e301b
KrebsCoder <lucasklopess@gmail.com>
2022-07-07 04:45:07 +0200

    modify: now the main struct is called philo istead of ph

--- philo/source/main.c
+++ philo/source/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 22:11:11 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/06/29 02:15:15 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/07 03:12:40 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	main(int argc, char *argv[])
 {
-	t_philo ph;
+	t_philo	philo;
 
-	ft_init(&ph, argc, argv);
-	create_philo(&ph);
+	ft_init(&philo, argc, argv);
+	create_philo(&philo);
 	return (0);
 }
