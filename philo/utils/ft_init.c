7b523d865d184cdf36542feb29e87d54f2e40c81
KrebsCoder <lucasklopess@gmail.com>
2022-07-08 04:41:32 +0200

    add: new variable initialization

--- philo/utils/ft_init.c
+++ philo/utils/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 23:02:40 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/06 03:48:18 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/08 04:23:38 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	ft_init(t_philo *philo, int argc, char *argv[])
 	philo->time_die = ft_atoi(argv[2]);
 	philo->time_eat = ft_atoi(argv[3]);
 	philo->time_sleep = ft_atoi(argv[4]);
+	philo->is_dead = -1;
+	philo->start_time_flag = 0;
 	if (argv[5])
 		philo->times_must_eat = ft_atoi(argv[5]);
 	else
