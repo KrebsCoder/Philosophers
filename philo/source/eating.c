74534e00ef078877c2b2fbea4d1d5d72ca63a7d2
KrebsCoder <lucasklopess@gmail.com>
2022-07-08 04:42:50 +0200

    remove: unnecessary commentaries

--- philo/source/eating.c
+++ philo/source/eating.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:19:36 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/07 03:51:01 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/08 03:34:10 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,8 @@ void	philo_is_eating(t_list *philo)
 	pthread_mutex_lock(&philo->next->fork);
 	printf("%ld ms %d has taken a fork\n", time_in_ms(philo), philo->id);
 	printf("%ld ms %d is eating\n", time_in_ms(philo), philo->id);
-	
-	// gettimeofday(&philo->philo->current_time, NULL);
-	// printf("-> %ld\n", philo->philo->current_time.tv_sec);
 	usleep(philo->philo->time_eat * 1000);
-	// gettimeofday(&philo->philo->current_time, NULL);
-	// printf("-> %ld\n", philo->philo->current_time.tv_sec);
+	gettimeofday(&philo->stopped_eating, NULL);
 	philo->eat_counter++;
 	pthread_mutex_unlock(&philo->fork);
 	pthread_mutex_unlock(&philo->next->fork);
