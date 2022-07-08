5e985c89eca8a2d435a6b71f2e2d7674f8af71e7
KrebsCoder <lucasklopess@gmail.com>
2022-07-08 04:40:48 +0200

    modify: now time_in_ms is working properly

--- philo/source/time_in_ms.c
+++ philo/source/time_in_ms.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 03:07:30 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/07 04:02:51 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/08 02:14:56 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ long	time_in_ms(t_list *philo)
 {
 	long	time_in_ms;
 	long	current_time_ms;
-	long	start_time_ms;
 
 	gettimeofday(&philo->philo->current_time, NULL);
-	current_time_ms = philo->philo->current_time.tv_usec;
-	start_time_ms = philo->philo->start_time.tv_usec;
-	time_in_ms = current_time_ms - start_time_ms;
-	return (time_in_ms / 1000);
+	current_time_ms = (philo->philo->current_time.tv_sec * 1000) + \
+		(philo->philo->current_time.tv_usec / 1000);
+	time_in_ms = current_time_ms - philo->philo->start_time;
+	return (time_in_ms);
 }
