05d59f301acd8684f5c055f8c80115aa2528c916
KrebsCoder <lucasklopess@gmail.com>
2022-07-08 04:42:21 +0200

    add: now sleeping routine checks if philo is dead

--- philo/source/sleeping.c
+++ philo/source/sleeping.c
@@ -6,7 +6,7 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:20:31 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/07 03:48:35 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/08 03:43:07 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 void	philo_is_sleeping(t_list *philo)
 {
+	struct timeval	time;
+	long			time_now;
+	long			time_stopped_eating;
+	
 	printf("%ld ms %d is sleeping\n", time_in_ms(philo), philo->id);
 	usleep(philo->philo->time_sleep * 1000);
+	gettimeofday(&time, NULL);
+	time_now = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	time_stopped_eating = (philo->stopped_eating.tv_sec * 1000) + \
+		(philo->stopped_eating.tv_usec / 1000);
+	if ((time_now - time_stopped_eating) > philo->philo->time_die)
+		philo->philo->is_dead = philo->id;
 }
