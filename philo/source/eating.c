7c0e361fd4971212ae6244b2ad0f83e5e5d4bbde
KrebsCoder <lucasklopess@gmail.com>
2022-07-14 03:23:46 +0200

    modify: now the the eating routine has flags

--- philo/source/eating.c
+++ philo/source/eating.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   eating.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:19:36 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/12 03:53:05 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/14 03:13:04 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,21 @@
 
 void	philo_is_eating(t_list *philo)
 {
-	long	first_fork;
-	long	second_fork;
+	struct timeval	time;
 
 	pthread_mutex_lock(&philo->fork);
 	printf("%ld ms %d has taken a fork\n", time_in_ms(philo), philo->id);
-	
-	first_fork = time_in_ms(philo);
-	
+	gettimeofday(&time, NULL);
+	philo->first_fork = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	philo->has_fork = 1;
 	pthread_mutex_lock(&philo->next->fork);
+	philo->has_fork = 0;
+	philo->first_fork = 0;
 	printf("%ld ms %d has taken a fork\n", time_in_ms(philo), philo->id);
-	
-	second_fork = time_in_ms(philo);
-	if (second_fork - first_fork > philo->philo->time_die)
-	{
-		philo->philo->is_dead = philo->id;
-		philo_is_dead(philo->philo);
-	}
-	
 	printf("%ld ms %d is eating\n", time_in_ms(philo), philo->id);
+	
 	usleep(philo->philo->time_eat * 1000);
+	
 	gettimeofday(&philo->stopped_eating, NULL);
 	philo->eat_counter++;
 	pthread_mutex_unlock(&philo->fork);
