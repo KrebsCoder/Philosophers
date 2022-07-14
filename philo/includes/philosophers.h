ae70e3402bcc09d516420c03ce885411838e3894
KrebsCoder <lucasklopess@gmail.com>
2022-07-14 03:19:24 +0200

    add: new functions to header

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 22:54:36 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/12 02:00:22 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/14 03:00:51 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,21 @@
 
 typedef struct s_list
 {
-	pthread_mutex_t	fork;
-	pthread_t		thread;
+	int				has_fork;
 	int				eat_counter;
 	int				id;
+	long			first_fork;
+	pthread_t		thread;
 	struct s_list	*previous;
 	struct s_list	*next;
 	struct s_philo	*philo;
 	struct timeval	stopped_eating;
+	pthread_mutex_t	fork;
 }	t_list;
 
 typedef struct s_philo
 {
+	int				someone_is_dead;
 	int				nbr_philos;
 	int				nbr_forks;
 	int				time_eat;
@@ -43,13 +46,13 @@ typedef struct s_philo
 	int				is_dead;
 	int				start_time_flag;
 	long			start_time;
-	struct timeval	current_time;
 	t_list			*list;
+	pthread_t		vigilant;
+	struct timeval	current_time;
 }	t_philo;
 
 void	check_arguments(int argc, char *argv[]);
 void	create_philo(t_philo *ph);
-void	*routine(void *philo);
 void	create_threads(t_philo *philo);
 void	thread_join(t_philo *philo);
 void	init_linked_list(t_philo *philo);
@@ -58,11 +61,14 @@ void	destroy_mutex(t_philo *philo);
 long	time_in_ms(t_list *philo);
 void	start_time(t_philo *philo);
 void	detach_thread(t_philo *philo);
-void	philo_is_dead(t_philo *philo);
+void	philo_is_dead(t_philo *philo, int philo_id);
 
+// Routines
 void	philo_is_eating(t_list *philo);
 void	philo_is_sleeping(t_list *philo);
 void	philo_is_thinking(t_list *philo);
+void	*routine(void *node);
+void	*routine_vigilant(void *node);
 
 // Utils
 int		ft_atoi(const char *str);
