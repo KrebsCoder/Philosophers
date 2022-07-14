5438eaa7842ef7c85736d2d4d28472f7d4601ec3
KrebsCoder <lucasklopess@gmail.com>
2022-07-14 04:16:52 +0200

    modify: now function works with philo_id

--- philo/source/philo_is_dead.c
+++ philo/source/philo_is_dead.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   philo_is_dead.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 02:41:23 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/12 02:02:04 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/14 04:16:30 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	philo_is_dead(t_philo *philo)
+void	philo_is_dead(t_philo *philo, int philo_id)
 {
 	pthread_mutex_t	is_dead;
 
 	pthread_mutex_init(&is_dead, NULL);
 	pthread_mutex_lock(&is_dead);
-	printf("%ld ms %d died\n", time_in_ms(philo->list), philo->is_dead);
+	printf("%ld ms %d died\n", time_in_ms(philo->list), philo_id);
 	detach_thread(philo);
 	pthread_mutex_unlock(&is_dead);
 	pthread_mutex_destroy(&is_dead);
