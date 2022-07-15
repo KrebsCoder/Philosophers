2a15f2abec7348be1d620602be194fdaf81cdbf6
KrebsCoder <lucasklopess@gmail.com>
2022-07-15 05:15:56 +0200

    add: now is_printing_mutex initializes on init_mutex

--- philo/source/init_mutex.c
+++ philo/source/init_mutex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_mutex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:23:30 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/07 03:24:00 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/15 04:25:48 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,8 @@ void	init_mutex(t_philo *philo)
 	while (i > 0)
 	{
 		pthread_mutex_init(&tmp->fork, NULL);
-		// printf("current: %d\n", tmp->id);
-		// printf("previous: %d\nnext: %d\n", tmp->previous->id, tmp->next->id);
 		tmp = tmp->next;
 		i--;
 	}
+	pthread_mutex_init(&philo->is_printing_mutex, NULL);
 }
