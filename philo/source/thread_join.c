ce2f1adf61a298231f870c5a8f11bff3c5ec66e1
KrebsCoder <lucasklopess@gmail.com>
2022-07-15 05:27:35 +0200

    modify: now vigilant is the last thread to join, so it has a bit of delay

--- philo/source/thread_join.c
+++ philo/source/thread_join.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:24:55 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/14 01:53:30 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/15 04:02:00 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	thread_join(t_philo *philo)
 
 	i = philo->nbr_forks;
 	tmp = philo->list;
-	pthread_join(tmp->philo->vigilant, NULL);
 	while (i > 0)
 	{
 		pthread_join(tmp->thread, NULL);
 		tmp = tmp->next;
 		i--;
 	}
+	pthread_join(tmp->philo->vigilant, NULL);
 }
