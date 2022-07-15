302d4fa4f0e45d934314a9e2c365081b9a8f571e
KrebsCoder <lucasklopess@gmail.com>
2022-07-15 05:15:20 +0200

    modify: renamed and refactored start_time function

--- philo/source/current_time.c
+++ philo/source/current_time.c
@@ -1,30 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   start_time.c                                       :+:      :+:    :+:   */
+/*   current_time.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 02:04:14 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/14 04:38:05 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/07/15 02:46:44 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	start_time(t_philo *philo)
+long	current_time(void)
 {
-	struct timeval	start_time;
-	// pthread_mutex_t	start_time_mutex;
+	struct timeval	current_time;
 
-	// pthread_mutex_init(&start_time_mutex, NULL);
-	pthread_mutex_lock(&philo->start_time_mutex);
-	if (philo->start_time_flag == 0)
-	{
-		philo->start_time_flag = 1;
-		gettimeofday(&start_time, NULL);
-		philo->start_time = (start_time.tv_sec * 1000) + \
-			(start_time.tv_usec / 1000);
-	}
-	pthread_mutex_unlock(&philo->start_time_mutex);
+	gettimeofday(&current_time, NULL);
+	return ((current_time.tv_sec * 1000) + (current_time.tv_usec / 1000));
 }
