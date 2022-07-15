7e94fa40f31cf72b90bbee4635b67c3686e25cd8
KrebsCoder <lucasklopess@gmail.com>
2022-07-15 05:53:01 +0200

    modify: now the philosopher is dying on a proper time

--- philo/source/eating.c
+++ philo/source/eating.c
@@ -6,31 +6,61 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:19:36 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/07/14 03:13:04 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/15 05:49:31 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
+static void	take_first_fork(t_list *philo);
+static void	take_second_fork(t_list *philo);
+
 void	philo_is_eating(t_list *philo)
 {
-	struct timeval	time;
-
-	pthread_mutex_lock(&philo->fork);
-	printf("%ld ms %d has taken a fork\n", time_in_ms(philo), philo->id);
-	gettimeofday(&time, NULL);
-	philo->first_fork = (time.tv_sec * 1000) + (time.tv_usec / 1000);
-	philo->has_fork = 1;
-	pthread_mutex_lock(&philo->next->fork);
-	philo->has_fork = 0;
-	philo->first_fork = 0;
-	printf("%ld ms %d has taken a fork\n", time_in_ms(philo), philo->id);
-	printf("%ld ms %d is eating\n", time_in_ms(philo), philo->id);
-	
+	if (philo->philo->someone_is_dead)
+		return ;
+	take_first_fork(philo);
+	if (philo->philo->someone_is_dead)
+		return ;
+	take_second_fork(philo);
+	philo->stopped_eating = current_time();
+	if (philo->philo->someone_is_dead)
+		return ;
 	usleep(philo->philo->time_eat * 1000);
-	
-	gettimeofday(&philo->stopped_eating, NULL);
 	philo->eat_counter++;
 	pthread_mutex_unlock(&philo->fork);
 	pthread_mutex_unlock(&philo->next->fork);
+}
+
+static void take_first_fork(t_list *philo)
+{
+	if (philo->philo->someone_is_dead)
+		return ;
+	pthread_mutex_lock(&philo->fork);
+	pthread_mutex_lock(&philo->philo->is_printing_mutex);
+	if (philo->philo->someone_is_dead)
+		return ;
+	printf("%ld ms %d has taken a fork\n", current_time() - philo->start_time, \
+		philo->id);
+	pthread_mutex_unlock(&philo->philo->is_printing_mutex);
+}
+
+static void	take_second_fork(t_list *philo)
+{
+	if (philo->philo->someone_is_dead)
+		return ;
+	pthread_mutex_lock(&philo->next->fork);
+	if (philo->philo->someone_is_dead)
+		return ;
+	pthread_mutex_lock(&philo->philo->is_printing_mutex);
+	if (philo->philo->someone_is_dead)
+		return ;
+	printf("%ld ms %d has taken a fork\n", current_time() - philo->start_time, \
+		philo->id);
+	pthread_mutex_unlock(&philo->philo->is_printing_mutex);
+	pthread_mutex_lock(&philo->philo->is_printing_mutex);
+	if (philo->philo->someone_is_dead)
+		return ;
+	printf("%ld ms %d is eating\n", current_time() - philo->start_time, philo->id);
+	pthread_mutex_unlock(&philo->philo->is_printing_mutex);
 }
--- philo/source/routine_vigilant.c
+++ philo/source/routine_vigilant.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 01:05:50 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/07/15 05:09:57 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/15 05:46:53 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*routine_vigilant(void *node)
 {
-	int				time_waiting_for_fork;
-	t_list			*philo;
+	int		time_last_eaten;
+	t_list	*philo;
 
 	philo = (t_list *)node;
 	while (!philo->philo->someone_is_dead)
 	{
 		pthread_mutex_lock(&philo->philo->is_printing_mutex);
-		time_waiting_for_fork = current_time() - philo->stopped_eating;
-		if (time_waiting_for_fork > philo->philo->time_die)
+		time_last_eaten = current_time() - philo->stopped_eating;
+		if (time_last_eaten >= philo->philo->time_die)
 		{
 			destroy_mutex(philo->philo);
 			philo->philo->someone_is_dead = 1;
