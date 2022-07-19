19f746da49a55b7f820dcc6f4e499ed49ab529be
KrebsCoder <lucasklopess@gmail.com>
2022-07-19 06:38:09 +0200

    modify: commented mutex because of a new _Atomic

--- philo/utils/check_death.c
+++ philo/utils/check_death.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 06:51:24 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/07/15 06:58:27 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/19 05:52:43 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	check_death(t_philo *philo)
 {
 	int	is_someone_dead;
 
-	pthread_mutex_lock(&philo->check_deaths);
+	//pthread_mutex_lock(&philo->check_deaths);
 	is_someone_dead = philo->someone_is_dead;
-	pthread_mutex_unlock(&philo->check_deaths);
+	//pthread_mutex_unlock(&philo->check_deaths);
 	return (is_someone_dead);
 }
