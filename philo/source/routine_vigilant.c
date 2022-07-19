532ddaa5251f05893b41d87340f78f85ecbf6663
KrebsCoder <lucasklopess@gmail.com>
2022-07-19 06:36:57 +0200

    modify: now the vigilant is with about 90% more performance

--- philo/source/routine_vigilant.c
+++ philo/source/routine_vigilant.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 01:05:50 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/07/15 07:12:56 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/07/19 06:13:57 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,23 @@ void	*routine_vigilant(void *node)
 	t_list	*philo;
 
 	philo = (t_list *)node;
+	//usleep(2100);
+	//printf("vigilant entrou\n");
 	while (!philo->philo->someone_is_dead)
 	{
 		pthread_mutex_lock(&philo->philo->is_printing_mutex);
 		time_last_eaten = current_time() - philo->stopped_eating;
 		if (time_last_eaten >= philo->philo->time_die)
 		{
-			pthread_mutex_lock(&philo->philo->check_deaths);
+			//pthread_mutex_lock(&philo->philo->check_deaths);
 			philo->philo->someone_is_dead = 1;
-			pthread_mutex_unlock(&philo->philo->check_deaths);
 			destroy_mutex(philo->philo);
+			//pthread_mutex_unlock(&philo->philo->check_deaths);
 			break ;
 		}
 		pthread_mutex_unlock(&philo->philo->is_printing_mutex);
 		philo = philo->next;
+		usleep(100);
 	}
 	philo_is_dead(philo->philo, philo->id);
 	return (NULL);
